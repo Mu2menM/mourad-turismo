bdf8a16085d9d7f4886271ec6f4b53f3caf44676
Mumen <mumen.murad@stud.fh-campuswien.ac.at>
2026-05-31 18:18:19 +0200

    Use Google Maps Embed API place mode for reliable pin marker

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -105,7 +105,7 @@ export default function Contact() {
       <div className="relative w-full">
         <iframe
           title="Mourad Turismo - Florianópolis"
-          src="https://maps.google.com/maps?q=Florianópolis,+SC,+Brazil&t=&z=13&ie=UTF8&iwloc=&output=embed"
+          src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8&q=Florianópolis,SC,Brazil&zoom=12"
           width="100%"
           height="450"
           style={{ border: 0, display: 'block' }}
