c5413f5014185791eecee523fed9c6a5e40ea59d
Mumen <mumen.murad@stud.fh-campuswien.ac.at>
2026-05-31 18:24:11 +0200

    Make scroll indicator clickable, scrolls to about section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -49,11 +49,15 @@ export default function Hero() {
       </div>
 
       {/* Scroll indicator */}
-      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 animate-bounce">
-        <div className="w-6 h-10 border-2 border-white/50 rounded-full flex justify-center pt-2">
+      <a
+        href="#about"
+        className="absolute bottom-8 left-1/2 -translate-x-1/2 animate-bounce cursor-pointer"
+        aria-label="Scroll down"
+      >
+        <div className="w-6 h-10 border-2 border-white/50 hover:border-white rounded-full flex justify-center pt-2 transition-colors">
           <div className="w-1.5 h-3 bg-white/70 rounded-full" />
         </div>
-      </div>
+      </a>
     </section>
   )
 }
